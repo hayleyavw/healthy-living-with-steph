9b69ee005024c4c9348a876ec0a6260b9ff4981e
Hayley van Waas <hayleyavw@gmail.com>
2019-01-08 10:26:08 +1300

    contact form now working

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const body_parser = require('body-parser');
+const validator = require("email-validator");
 
 const app = express();
 app.use(express.static('templates'));
 app.use(express.static('static'));
+app.use(body_parser.json());
 
 
 app.get('/', function (req, res) {
@@ -10,5 +13,29 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000, function() {
-	console.log("Example app listening for cats");
+	console.log("Server running on 3000");
 });
+
+var mailgun = require("mailgun-js");
+var api_key = 'key-5f88a965cacbaa9907fc3a2f79083f7a';
+var DOMAIN = 'hayleyavw.com';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
+
+app.post('/', function(req, res) {
+	var email = req.body.email;
+	if (validator.validate(email) == false) {
+		return res.status(400).send();
+	}
+	var data = {
+	  from: email,
+	  to: 'contact@hayleyavw.com',
+	  subject: 'Message from ' + req.body.name,
+	  text: req.body.message
+	};
+
+	// mailgun.messages().send(data, function (error, body) {
+	  // console.log(body);
+	// });
+
+	res.status(200).send();
+})
